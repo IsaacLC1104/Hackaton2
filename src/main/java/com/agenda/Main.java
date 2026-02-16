cc3c220ff2595417804c48d6e267ecdeafcc4d2c
Isaac <isaclopezcar@gmail.com>
2026-02-16 16:31:12 -0300

    Main

--- src/main/java/com/agenda/Main.java
+++ src/main/java/com/agenda/Main.java
@@ -1,8 +1,14 @@
 package com.agenda;
 
+import com.agenda.vista.MenuConsola;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        MenuConsola menu = new MenuConsola();
+        menu.mostrarMenu();
+        menu.cerrar();
 
-        System.out.println("Hello world!");
     }
 }
